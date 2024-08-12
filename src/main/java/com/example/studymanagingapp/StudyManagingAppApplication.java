5a6b13e7fdb3ffcfd40f2793134de01e483d7c2a
Tomk99 <tcsavas99@gmail.com>
2024-08-12 19:43:10 +0200

    Cubix Intermediate Course Homework

---
+++ src/main/java/com/example/studymanagingapp/StudyManagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.studymanagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyManagingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyManagingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studymanagingapp/StudyManagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studymanagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyManagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
